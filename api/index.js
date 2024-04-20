fb2a1b5abdfe0ff5298b31e057d4b0240a8edfee
Shahzain Hassan <shahzain.hassan.1@gmail.com>
2024-04-20 18:06:22 +0500

    Login API completed

--- api/index.js
+++ api/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
-const bcrypt=require('bcrypt')
-const app = express();
-const salt=bcrypt.genSaltSync(10);
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-app.use(cors());
+const app = express();
+const salt = bcrypt.genSaltSync(10);
+const secret = "abcdef";
+
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect(
@@ -18,11 +21,25 @@ app.post("/register", async (req, res) => {
   try {
     const userDoc = await User.create({
       username,
-      password:bcrypt.hashSync(password,salt),
+      password: bcrypt.hashSync(password, salt),
     });
     res.json(userDoc);
   } catch (error) {
-    res.status(400).json(error)
+    res.status(400).json(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("Ok");
+    });
+  }else{
+    res.status(400).json("wrong credentials")
   }
 });
 
